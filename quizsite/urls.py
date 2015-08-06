175813caad9842d0052fe16ebc37d2613f6e49d7
gmunnich <gabriela.munnich@gmail.com>
2015-08-06 10:36:07 +0200

    added urls

--- quizsite/urls.py
+++ quizsite/urls.py
@@ -1,3 +1,13 @@
+from django.conf.urls import url
+from quiz import views
+
+urlpatterns = [
+        url("^$", views.startpage),
+        url(r"^quiz/[a-z-]+/$", views.quiz),
+        url(r"^quiz/[a-z-]+/question/[0-9]/$", views.question),
+        url(r"^quiz/[a-z-]+/completed/$", views.completed),
+        ]
+
 """quizsite URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
