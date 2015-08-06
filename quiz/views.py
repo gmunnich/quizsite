c91c24e7238d79288ccfd3a9278c2d122d3e31d8
gmunnich <gabriela.munnich@gmail.com>
2015-08-06 10:56:07 +0200

    added data

--- quiz/views.py
+++ quiz/views.py
@@ -1,7 +1,30 @@
+#coding: utf-8
+
 from django.shortcuts import render
 
+quizzes = {
+	"klassiker": {
+   		"name": u"Klassiska böcker",
+	   	"description": u"Hur bra kan du dina klassiker?"
+	},
+	"fotboll": {
+	   	"name": u"Största fotbollslagen",
+	   	"description": u"Kan du dina lag?"
+	},
+	"kanda-hackare": {
+	    	"name": u"Världens mest kända hackare",
+	    	"description": u"Hackerhistoria är viktigt, kan du den?"	},
+}
+
+
+
+
 def startpage(request):
-	return render(request, "quiz/startpage.html")
+	context = {
+		"quizzes": quizzes,
+	}
+	return render(request, "quiz/startpage.html", context)
+
 
 def quiz(request):
     return render(request, "quiz/quiz.html")
