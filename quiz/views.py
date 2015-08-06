f674d1fdf9f2cab2832e64d86d18d41095b20d72
gmunnich <gabriela.munnich@gmail.com>
2015-08-06 10:44:57 +0200

    made it work

--- quiz/views.py
+++ quiz/views.py
@@ -1,16 +1,16 @@
 from django.shortcuts import render
 
 def startpage(request):
-	return render(request, "quiz/startsidaquiz.html")
+	return render(request, "quiz/startpage.html")
 
 def quiz(request):
-    return render(request, "quiz/quiz1start.html")
+    return render(request, "quiz/quiz.html")
 
 def question(request): 
-	return render(request, "quiz/quiz1.html")
+	return render(request, "quiz/question.html")
 
 def completed(request):
-	return render(request, "quiz/quiz1resultat")
+	return render(request, "quiz/completed")
 
 
 # Create your views here.
