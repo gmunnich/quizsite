af2745dd538cdecc4a4027c5401c9ac791333cd9
gmunnich <gabriela.munnich@gmail.com>
2015-08-07 13:39:32 +0200

    ladda upp

--- quizsite/settings.py
+++ quizsite/settings.py
@@ -79,13 +79,14 @@ WSGI_APPLICATION = 'quizsite.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/1.8/ref/settings/#databases
-
+import dj_database_url
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+        'default': dj_database_url.config(
+        default='sqlite:////{0}'.format(os.path.join(BASE_DIR,
+'db.sqlite3'))
+    )
 }
+
 
 
 # Internationalization
@@ -106,3 +107,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = 'staticfiles'
