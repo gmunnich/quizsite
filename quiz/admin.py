e72dd44936ad2540073809097980f076d4589232
gmunnich <gabriela.munnich@gmail.com>
2015-08-07 09:36:03 +0200

    databas

--- quiz/admin.py
+++ quiz/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
+from django.contrib import admin
+from quiz.models import Quiz, Question
+admin.site.register(Quiz)
+admin.site.register(Question)
+
+
 # Register your models here.
--- quizsite/urls.py
+++ quizsite/urls.py
@@ -1,3 +1,6 @@
+from django.contrib import admin
+from django.conf.urls import include
+
 from django.conf.urls import url
 from quiz import views
 
@@ -6,5 +9,6 @@ urlpatterns = [
         url(r"^quiz/([a-z-]+)/$", views.quiz,name="quiz_page"),
         url(r"^quiz/([a-z-]+)/question/([0-9])/$", views.question,name="question_page"),
         url(r"^quiz/([a-z-]+)/completed/$", views.completed,name="completed_page"),
+        url(r'^admin/', include(admin.site.urls)),
         ]
 
